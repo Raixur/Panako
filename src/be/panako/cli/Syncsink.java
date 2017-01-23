8e8c5e0e0fbb11c853c6a9edff187fb2472864c7
Anatoliy Bernatovich <raixur@gmail.com>
2017-01-23 13:08:53 +0200

    Initial commit

---
+++ src/be/panako/cli/Syncsink.java
@@ -0,0 +1,91 @@
+/***************************************************************************
+*                                                                          *
+* Panako - acoustic fingerprinting                                         *
+* Copyright (C) 2014 - 2015 - Joren Six / IPEM                             *
+*                                                                          *
+* This program is free software: you can redistribute it and/or modify     *
+* it under the terms of the GNU Affero General Public License as           *
+* published by the Free Software Foundation, either version 3 of the       *
+* License, or (at your option) any later version.                          *
+*                                                                          *
+* This program is distributed in the hope that it will be useful,          *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+* GNU Affero General Public License for more details.                      *
+*                                                                          *
+* You should have received a copy of the GNU Affero General Public License *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>     *
+*                                                                          *
+****************************************************************************
+*    ______   ________   ___   __    ________   ___   ___   ______         *
+*   /_____/\ /_______/\ /__/\ /__/\ /_______/\ /___/\/__/\ /_____/\        *
+*   \:::_ \ \\::: _  \ \\::\_\\  \ \\::: _  \ \\::.\ \\ \ \\:::_ \ \       *
+*    \:(_) \ \\::(_)  \ \\:. `-\  \ \\::(_)  \ \\:: \/_) \ \\:\ \ \ \      *
+*     \: ___\/ \:: __  \ \\:. _    \ \\:: __  \ \\:. __  ( ( \:\ \ \ \     *
+*      \ \ \    \:.\ \  \ \\. \`-\  \ \\:.\ \  \ \\: \ )  \ \ \:\_\ \ \    *
+*       \_\/     \__\/\__\/ \__\/ \__\/ \__\/\__\/ \__\/\__\/  \_____\/    *
+*                                                                          *
+****************************************************************************
+*                                                                          *
+*                              Panako                                      *
+*                       Acoustic Fingerprinting                            *
+*                                                                          *
+****************************************************************************/
+
+
+package be.panako.cli;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.File;
+
+import javax.swing.UIManager;
+
+import be.panako.ui.syncsink.SyncSinkFrame;
+
+public class Syncsink extends Application {
+	
+	@Override
+	public void run(final String... args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				} catch (Exception e) {
+				}
+				SyncSinkFrame frame = new SyncSinkFrame();
+				frame.setSize(800, 600);
+				frame.setVisible(true);
+				Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+				frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+				
+				//add the files
+				int i = 0;
+				for(File file : getFilesFromArguments(args)){
+					frame.openFile(file,i);
+					i++;
+				}
+			}
+		});
+	}
+	
+	@Override
+	public String description() {
+		return "Shows a user interface to synchronize audio/video/data files with respect to the reference.";
+	}
+
+	@Override
+	public String synopsis() {
+		return "[reference] [others...]";
+	}
+
+	@Override
+	public boolean needsStorage() {
+		return false;
+	}
+
+	@Override
+	public boolean writesToStorage() {
+		return false;
+	}
+}
